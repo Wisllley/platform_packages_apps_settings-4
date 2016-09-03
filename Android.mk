427a8a76ba85e28d3d9d3a90fd334faa9691f392
Alex <mazdarider23@gmail.com>
2016-09-03 01:22:42 -0400

    Integrate Tentacles into Settings
    Thanks to @camcory for the manifest part, big help there
    
    Team-OctOS modified Dirty Tweaks for Tentacles
    
    Change-Id: I16a9a2b7648717b81f20667f3256413bf2ca4621

--- Android.mk
+++ Android.mk
@@ -29,13 +29,19 @@ LOCAL_STATIC_JAVA_LIBRARIES += \
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
-        $(call all-java-files-under, src)
+        $(call all-java-files-under, src) \
+        $(call all-java-files-under, ../Tentacles/src)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v7/preference/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res
+
+LOCAL_RESOURCE_DIR += packages/apps/Tentacles/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+      --extra-packages com.teamoctos.tentacles
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
