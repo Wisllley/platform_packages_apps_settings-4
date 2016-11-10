DirtyUnicorns/android_packages_apps_Settings

Use full featured color picker for notification light
Change-Id: I27de0c3cae03f277ecf5d5d89b81792d85083832

src/com/android/settings/batterylight/BatteryLightSettings.java
@@ -36,7 +36,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 
-import com.dirtyunicorns.dutweaks.preference.SystemSettingSwitchPreference
+import com.dirtyunicorns.dutweaks.preference.SystemSettingSwitchPreference;
 
 import java.util.List;
 import java.util.ArrayList;

src/com/android/settings/batterylight/NotificationLightSettings.java
@@ -49,7 +49,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 
-import com.dirtyunicorns.dutweaks.preference.SystemSettingSwitchPreference
+import com.dirtyunicorns.dutweaks.preference.SystemSettingSwitchPreference;
 import com.android.settings.preferences.AppSelectListPreference;
 import com.android.settings.preferences.AppSelectListPreference.PackageItem;
 

Wisllley/platform_packages_apps_settings-4

Change-Id: Ie4d00f5d2dfd7c667c398cad3d0f78fd1188b25b

src/com/android/settings/batterylight/BatteryLightSettings.java
@@ -36,7 +36,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 
-import com.teamoctos.tentacles.preference.SystemSettingSwitchPreference
+import com.teamoctos.tentacles.preference.SystemSettingSwitchPreference;
 
 import java.util.List;
 import java.util.ArrayList;

src/com/android/settings/batterylight/NotificationLightSettings.java
@@ -49,7 +49,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 
-import com.teamoctos.tentacles.preference.SystemSettingSwitchPreference
+import com.teamoctos.tentacles.preference.SystemSettingSwitchPreference;
 import com.android.settings.preferences.AppSelectListPreference;
 import com.android.settings.preferences.AppSelectListPreference.PackageItem;
 