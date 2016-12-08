DirtyUnicorns/android_packages_apps_Settings

[QUICK SETTINGS] Expanded Desktop tile longpress action
- Fix sync tile longpress

Change-Id: I9aba0a9bf2b211267d5c2a5a22537cc669b179bb

src/com/android/settings/SettingsActivity.java
@@ -129,6 +129,7 @@ import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
 import com.dirtyunicorns.dutweaks.DirtyTweaks;
+import com.dirtyunicorns.dutweaks.fragments.ExpandedDesktop;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -350,6 +351,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
             DirtyTweaks.class.getName(),
+            ExpandedDesktop.class.getName(),
     };
 
 

Wisllley/platform_packages_apps_settings-4

Change-Id: Id5bf122d76e4663726c74b05dc4752f8946e181e

src/com/android/settings/SettingsActivity.java
@@ -136,6 +136,7 @@ import com.android.settingslib.drawer.Tile;
 
 import java.net.URISyntaxException;
 import com.teamoctos.tentacles.Tentacles;
+import com.teamoctos.tentacles.fragments.ExpandedDesktop;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -360,7 +361,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
-            Tentacles.class.getName()
+            Tentacles.class.getName(),
+            ExpandedDesktop.class.getName(),
     };
 
 