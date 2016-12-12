d90ceb1978ad7803ff07f482b957d7f5cd0ad804
hedwig34 <hedwig34@gmail.com>
2016-12-12 16:18:09 -0500

    settings: Clean up device_info_settings
    Change-Id: Ida4a93dad40e2f6ba3032f79585c5b2a18408be1

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -82,7 +82,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String QGP_VERSION_PATH = "/persist/speccfg/qgpversion";
     private static final String KEY_MOD_VERSION = "mod_version";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
-    private static final String KEY_MOD_API_LEVEL = "mod_api_level";
+    //private static final String KEY_MOD_API_LEVEL = "mod_api_level";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -148,8 +148,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         }
         setValueSummary(KEY_MOD_VERSION, "ro.to.version");
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
-        setExplicitValueSummary(KEY_MOD_API_LEVEL, constructApiLevelString());
-        findPreference(KEY_MOD_API_LEVEL).setEnabled(true);
+        //setExplicitValueSummary(KEY_MOD_API_LEVEL, constructApiLevelString());
+        //findPreference(KEY_MOD_API_LEVEL).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
